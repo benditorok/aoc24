6701d9c9db061f3e4e6674948597de0dea568340
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-12-01 17:51:44 +0100

    Better solution

--- aoc24-day1/src/main.rs
+++ aoc24-day1/src/main.rs
@@ -1,4 +1,5 @@
-fn setup() -> (Vec<i32>, Vec<i32>) {
+fn main() {
+    // Setup
     let mut left_list: Vec<i32> = Vec::new();
     let mut right_list: Vec<i32> = Vec::new();
     let input = include_str!("input.txt");
@@ -12,54 +13,40 @@ fn setup() -> (Vec<i32>, Vec<i32>) {
         right_list.push(right);
     }
 
-    (left_list, right_list)
+    left_list.sort();
+    right_list.sort();
+
+    // Part one
+    p1(&left_list, &right_list);
+
+    // Part two
+    p2(&left_list, &right_list);
+
+    // Results
+    // Sum: 2113135
+    // Similarity score: 19097157
 }
 
-fn main() {
-    // Part one
-    let (mut left_list, mut right_list) = setup();
-
+fn p1(left_list: &[i32], right_list: &[i32]) {
     let mut sum = 0;
 
-    while !left_list.is_empty() && !right_list.is_empty() {
-        let left_min_idx = left_list
-            .iter()
-            .enumerate()
-            .min_by_key(|(_, &val)| val)
-            .map(|(idx, _)| idx)
-            .unwrap();
-        let right_min_idx = right_list
-            .iter()
-            .enumerate()
-            .min_by_key(|(_, &val)| val)
-            .map(|(idx, _)| idx)
-            .unwrap();
+    if left_list.len() != right_list.len() {
+        panic!("Lists are not the same length");
+    }
 
-        let left_min = left_list.remove(left_min_idx);
-        let right_min = right_list.remove(right_min_idx);
-
-        sum += (left_min - right_min).abs();
+    for i in 0..left_list.len() {
+        sum += (left_list[i] - right_list[i]).abs();
     }
 
     println!("Sum: {}", sum);
+}
 
-    // Part two
-    let (mut left_list, right_list) = setup();
-
+fn p2(left_list: &[i32], right_list: &[i32]) {
     let mut similarity_score = 0;
 
-    while !left_list.is_empty() {
-        let left_min_idx = left_list
-            .iter()
-            .enumerate()
-            .min_by_key(|(_, &val)| val)
-            .map(|(idx, _)| idx)
-            .unwrap();
-
-        let left_min = left_list.remove(left_min_idx);
-        let right_count = right_list.iter().filter(|&x| *x == left_min).count();
-
-        similarity_score += right_count as i32 * left_min;
+    for left_item in left_list {
+        similarity_score +=
+            right_list.iter().filter(|&x| *x == *left_item).count() as i32 * left_item;
     }
 
     println!("Similarity score: {}", similarity_score);
