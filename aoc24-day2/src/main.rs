8dfd31d93c7ca89df2cddd7a1fb9b86b9351e010
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-12-02 08:06:13 +0100

    Save progress on part 2

--- aoc24-day2/src/main.rs
+++ aoc24-day2/src/main.rs
@@ -12,12 +12,15 @@ fn main() {
 
     // Part one
     p1(&list);
+    p2(&mut list);
 }
 
 fn p1(list: &[Vec<i32>]) {
     let mut safe_count = 0;
 
     for line in list {
+        // TODO this need to be changed because we could remove the second item
+        // and then it might be valid
         let ascending = line[0] < line[1];
         let mut valid = true;
         let mut i = 0;
@@ -35,6 +38,60 @@ fn p1(list: &[Vec<i32>]) {
             while valid && i < end {
                 if line[i] <= line[i + 1] || !valid_difference(line[i], line[i + 1]) {
                     valid = false;
+                }
+
+                i += 1;
+            }
+        }
+
+        if valid {
+            safe_count += 1;
+        }
+    }
+
+    println!("Safe count: {}", safe_count);
+}
+
+fn p2(list: &mut [Vec<i32>]) {
+    let mut safe_count = 0;
+
+    for mut line in list {
+        let ascending = line[0] < line[1];
+        let mut valid = true;
+        let mut i = 0;
+        let mut end = line.len() - 1;
+        let mut removed_one = false;
+
+        if ascending {
+            while valid && i < end {
+                if line[i] >= line[i + 1] || !valid_difference(line[i], line[i + 1]) {
+                    if !removed_one {
+                        // Remove [i] and check again
+                        line.remove(i);
+                        i = 0;
+                        end -= 1;
+                        removed_one = true;
+                        continue;
+                    } else {
+                        valid = false;
+                    }
+                }
+
+                i += 1;
+            }
+        } else {
+            while valid && i < end {
+                if line[i] <= line[i + 1] || !valid_difference(line[i], line[i + 1]) {
+                    if !removed_one {
+                        // Remove [i] and check again
+                        line.remove(i);
+                        i = 0;
+                        end -= 1;
+                        removed_one = true;
+                        continue;
+                    } else {
+                        valid = false;
+                    }
                 }
 
                 i += 1;
